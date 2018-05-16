f9f4de9919a99e7af533875704b1f641ec2cae25
Hezi Shahmoon <hshahmoon@apple.com>
2018-05-16 10:25:23 +0300

    Fix full screen and hide cursor
    Signed-off-by: Hezi Shahmoon <hshahmoon@apple.com>

--- src/index.js
+++ src/index.js
@@ -2,11 +2,40 @@
 
 var controls;
 
+var button = document.createElement("BUTTON");
+button.innerText = "Full Screen";
+var bVisible = true;
+button.onclick = function full() {
+  button.remove();
+  THREEx.FullScreen.request();
+  $("body").css('cursor', 'none');
+};
+document.addEventListener('webkitfullscreenchange', function(e) {
+  showHide(e);
+}, false);
+document.addEventListener('mozfullscreenchange', function(e) {
+  showHide(e);
+}, false);
+document.addEventListener('fullscreenchange', function(e) {
+  showHide(e);
+}, false);
+function showHide(e) {
+  var b = document.getElementById('fullscreen');
+  if(!bVisible)
+  {
+    document.body.prepend( button );
+    $("body").css('cursor', '');
+  }
+  bVisible = !bVisible;
+}
+document.body.appendChild( button );
+
 var THREE = require('three')
    ,Vex = require('vexflow')
    ,RoundedBoxGeometry = require('three-rounded-box')(THREE) //pass your instance of three
    ,OBJLoader = require('three-obj-loader')
-   ,OrbitControls = require('three-orbit-controls')(THREE);
+   ,OrbitControls = require('three-orbit-controls')(THREE)
+   ,$ = require('jquery');
 
   //, THREEx = require('threex');
 
@@ -25,9 +54,20 @@ controls = new OrbitControls(camera, renderer.domElement);
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
 //Create a PointLight and turn on shadows for the light
 var light = new THREE.PointLight( 0xffffff, 1, 100 );
-light.position.set( -10, 10, 0 );
+light.position.set( 0, 10, 5 );
 light.castShadow = true;            // default false
 scene.add( light );
 //Set up shadow properties for the light
@@ -36,7 +76,7 @@ light.shadow.mapSize.height = 512; // default
 light.shadow.camera.near = 0.5;       // default
 light.shadow.camera.far = 500      // default
 
-var geometry = new RoundedBoxGeometry( 4, 4, 4, 1, 16);
+var geometry = new RoundedBoxGeometry( 3, 3, 3, .1, 16);
 var material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 cube.castShadow = true; //default is false
@@ -46,8 +86,8 @@ scene.add( cube );
 camera.position.z = 5;
 
 //Create a helper for the shadow camera (optional)
-var helper = new THREE.CameraHelper( light.shadow.camera );
-scene.add( helper );
+//var helper = new THREE.CameraHelper( light.shadow.camera );
+//scene.add( helper );
 
 function animate() {
   requestAnimationFrame( animate );
@@ -66,3 +106,11 @@ vexRenderer.resize(500, 500);
 
 // And get a drawing context:
 var context = vexRenderer.getContext();
+// Create a stave at position 10, 40 of width 400 on the canvas.
+var stave = new VF.Stave(10, 40, 400);
+
+// Add a clef and time signature.
+stave.addClef("treble").addTimeSignature("4/4");
+
+// Connect it to the rendering context and draw!
+stave.setContext(context).draw();
